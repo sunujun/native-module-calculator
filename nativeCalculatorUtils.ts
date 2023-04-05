5a1981e9688d978b06cceb7b6af74b7159185e5c
seongjun.oh <tiger8708@gmail.com>
2023-04-05 22:55:27 +0900

    계산기 View 구현

---
+++ nativeCalculatorUtils.ts
@@ -0,0 +1,9 @@
+import {NativeModules} from 'react-native';
+
+export const executeCalculator = (
+  action: 'plus' | 'minus' | 'divide' | 'multiply',
+  numbA: number,
+  numbB: number,
+): Promise<number> => {
+  return NativeModules.CalculatorModule.executeCalc(action, numbA, numbB);
+};
